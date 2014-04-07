cf077918256a35c69c55d53a56c0fa0d1667a51d
einsteinly <lieinsteinxi@gmail.com>
2014-04-07 01:27:03 +0000

    roots for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Ex1::Application.routes.draw do
   resources :ideas
 
-  match '/ideas' , to: 'static_pages#ideas' , via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'ideas#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
