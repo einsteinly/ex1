1277e07f1c4aec29e91b41f663ba5e13e6dec44d
einsteinly <lieinsteinxi@gmail.com>
2014-04-22 11:21:16 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Ex1::Application.routes.draw do
   resources :comments
 
   resources :ideas
-
+  match '/comments', to: 'comments#index', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'ideas#index'
-  root 'comments#index'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'comments/' => 'comments#index'
+
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
